fdd6ac79a87e257d9e8c60a9604082c30799a29a
brandaoplaster <brandaoplaster@gmail.com>
2024-06-09 22:40:27 -0300

    feat: Adding basic component edit category

---
+++ src/features/categories/EditCategory.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export const EditCategory = () => {
+  <Box>
+    <Typography variant="h1" component="h1">
+      Category Edit
+    </Typography>
+  </Box>;
+};
