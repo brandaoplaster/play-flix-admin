323aac0e26433f9f1a9c3b76a576aa5ca57da104
brandaoplaster <brandaoplaster@gmail.com>
2024-06-09 22:40:37 -0300

    feat: Adding basic component list category

---
+++ src/features/categories/ListCaegory.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export const CategoryList = () => {
+  <Box>
+    <Typography variant="h1" component="h1">
+      Category List
+    </Typography>
+  </Box>;
+};
