3fb46c4ae25734a1c78ba7c5a770cfa8434cada7
brandaoplaster <brandaoplaster@gmail.com>
2024-06-09 22:40:16 -0300

    feat: Adding basic component create category

---
+++ src/features/categories/CreateCategory.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export const CreateCategory = () => {
+  return (
+    <Box>
+      <Typography variant="h1" component="h1">
+        Create Category
+      </Typography>
+    </Box>
+  );
+};
