bd0115348b96146b9c531088a6e616f7338d0e63
brandaoplaster <brandaoplaster@gmail.com>
2024-06-10 22:45:56 -0300

    feat: [WIP] in category list page

--- src/features/categories/ListCaegory.tsx
+++ src/features/categories/ListCaegory.tsx
@@ -1,8 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const CategoryList = () => {
   return (
-    <Box>
+    <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/categories/create"
+          style={{ marginBottom: "1rem" }}
+        >
+          New Category
+        </Button>
+      </Box>
       <Typography variant="h1" component="h1">
         Category List
       </Typography>
