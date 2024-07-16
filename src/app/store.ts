30ff99b117febe7f58ae2f5321cd676517abde5d
brandaoplaster <brandaoplaster@gmail.com>
2024-07-15 22:51:42 -0300

    feat: Adding apiSlice in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import categorySlice from "../features/categories/categorySlice";
+import { apiSlice } from "../features/api/apiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     categories: categorySlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
 });
 
