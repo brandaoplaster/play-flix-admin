b608b593e75593eda49791a0cc09d7779f61469e
brandaoplaster <brandaoplaster@gmail.com>
2024-07-19 23:34:32 -0300

    chore: Remove unused code

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,11 +1,5 @@
 import { Box, Button, IconButton, Typography } from "@mui/material";
-import {
-  DataGrid,
-  GridColDef,
-  GridRenderCellParams,
-  GridRowsProp,
-  GridToolbar,
-} from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import {
@@ -19,71 +13,8 @@ export const CategoryList = () => {
   const categories = useAppSelector(selectCategories);
   const dispatch = useAppDispatch();
 
-  const componentProps = {
-    toolbar: {
-      showQuickFilter: true,
-      quickFilterProps: { debounceMs: 500 },
-    },
-  };
-
-  const rows: GridRowsProp = data
-    ? data.data.map((category) => ({
-        id: category.id,
-        name: category.name,
-        description: category.description,
-        isActive: category.is_active,
-        createdAt: category.created_at,
-      }))
-    : [];
-
-  const columns: GridColDef[] = [
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      renderCell: renderNameCell,
-    },
-    {
-      field: "IsActive",
-      headerName: "Status",
-      flex: 1,
-      renderCell: renderIsActiveCell,
-    },
-  ];
-
   function handleDeleteCategory(id: string) {
     dispatch(deleteCategory(id));
-  }
-
-  function renderIsActiveCell(row: GridRenderCellParams) {
-    return (
-      <Typography color={row.value ? "primary" : "secondary"}>
-        {row.value ? "Active" : "Inactive"}
-      </Typography>
-    );
-  }
-
-  function renderNameCell(rowData: GridRenderCellParams) {
-    return (
-      <Link
-        style={{ textDecoration: "none" }}
-        to={`/categories/edit${rowData.id}`}
-      >
-        <Typography color="primary">{rowData.value}</Typography>
-      </Link>
-    );
-  }
-
-  function renderActionsCell(params: GridRenderCellParams) {
-    return (
-      <IconButton
-        color="secondary"
-        onClick={handleDeleteCategory(params.value)}
-        aria-label="delete"
-      >
-        <DeleteIcon />
-      </IconButton>
-    );
   }
 
   return (
@@ -100,7 +31,7 @@ export const CategoryList = () => {
         </Button>
       </Box>
       <Box sx={{ display: "flex", height: 600 }}>
-        <DataGrid
+        {/* <DataGrid
           rows={rows}
           columns={columns}
           disableColumnFilter={true}
@@ -110,7 +41,7 @@ export const CategoryList = () => {
           componentsProps={componentProps}
           components={{ Toolbar: GridToolbar }}
           rowsPerPageOptions={[2, 20, 50, 100]}
-        />
+        /> */}
       </Box>
     </Box>
   );
