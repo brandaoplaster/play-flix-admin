e812d627249193064a41f24cf0309aeea1db1174
brandaoplaster <brandaoplaster@gmail.com>
2024-07-30 22:33:24 -0300

    feat: Adding hook useGetCastMembersQuery

--- src/features/cast/castMembersSlice.ts
+++ src/features/cast/castMembersSlice.ts
@@ -1,12 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { apiSlice } from "../api/apiSlice";
-import {
-  CastMember,
-  CastMemberParams,
-  Result,
-  Results,
-} from "../../types/CastMembers";
+import { CastMember, CastMemberParams, Results } from "../../types/CastMembers";
 
 const endpointUrl: string = "/cast_members";
 
@@ -20,7 +15,44 @@ export const initialState: CastMember = {
 };
 
 function parseQueryParams(params: CastMemberParams) {
-  return null;
+  const query = new URLSearchParams();
+
+  if (params.page) {
+    query.append("page", params.page.toString());
+  }
+
+  if (params.perPage) {
+    query.append("per_page", params.perPage.toString());
+  }
+
+  if (params.search) {
+    query.append("search", params.search);
+  }
+
+  if (params.type) {
+    query.append("is_active", params.type.toString());
+  }
+
+  return query.toString();
 }
 
-export const castMembersSlice = apiSlice.injectEndpoints({});
+function getCastMembers(params: CastMemberParams) {
+  const { page = 1, perPage = 10, search, type } = params;
+  return `${endpointUrl}?${parseQueryParams({
+    page,
+    perPage,
+    search,
+    type,
+  })}`;
+}
+
+export const castMembersSlice = apiSlice.injectEndpoints({
+  endpoints: ({ query }) => ({
+    getCastMembers: query<Results, CastMemberParams>({
+      query: getCastMembers,
+      providesTags: ["CastMembers"],
+    }),
+  }),
+});
+
+const { useGetCastMembersQuery } = castMembersSlice;
